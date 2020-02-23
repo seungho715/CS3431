c73701a56418fbf80a6b0a8574f86e2dba3378f9
Ken Snoddy <ksnoddy@wpi.edu>
2020-02-23 18:45:35 -0500

    Added Part 3

---
+++ Reporting.java
@@ -0,0 +1,557 @@
+//Phase 3 - Group 10 - Ken Snoddy, Seungho Lee
+
+import java.util.Scanner;
+import java.sql.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Reporting {
+    public static void main(String[] argv){
+        int numArgs = argv.length;
+
+        if(numArgs <2 || numArgs > 3)
+        {
+            System.out.println("Wong num of arguments");
+            System.out.println("Example: >java Reporting <username> <password> [option]");
+            System.exit(-1);
+        }
+        String username = argv[0];
+        String password = argv[1];
+
+        Connection c = connectToDatabase(username, password);
+
+        if(numArgs == 2)
+        {
+            System.out.println("print everything");
+            reportUserOptions();
+        }
+        else
+        {
+            Scanner s = new Scanner(System.in);
+            int tries;
+            switch(Integer.parseInt(argv[2])){
+                case 1:
+                    String regex = "^[0-9]{3}-[0-9]{2}-[0-9]{4}$";
+                    Pattern pat = Pattern.compile(regex);
+                    String patSSN = null;
+
+                    tries = 5;
+
+                    while(tries > 0)
+                    {
+                        System.out.println("Enter Patient SSN (Example: 000-000-0000): ");
+                        patSSN = s.nextLine();
+
+                        Matcher m = pat.matcher(patSSN);
+                        if(!m.matches())
+                        {
+                            System.out.println("SSN must be in format " +tries-- + " tries left");
+                        }
+                        else
+                            break;
+                        if(tries == 0)
+                        {
+                            System.out.println("Too many tries...");
+                            try{
+                                c.close();
+                            }catch(SQLException e){
+                                e.printStackTrace();
+                            }
+                            System.exit(-1);
+                        }
+                    }
+
+                    reportPatientBasicInfo(c, patSSN);
+
+                    try{
+                        c.close();
+                    }catch(SQLException e){
+                        e.printStackTrace();
+                    }
+
+                    break;
+
+                case 2:
+                    String docID = null;
+                    tries = 5;
+
+                    while(tries >0)
+                    {
+                        System.out.println("Enter Doctor ID: ");
+                        docID = s.nextLine();
+
+                        if(isInteger(docID) == -1)
+                            System.out.println("DoctorID must be valid " + tries-- + " tries left");
+                        else
+                            break;
+                        if(tries == 0)
+                        {
+                            System.out.println("Too many tries");
+                            try{
+                                c.close();
+                            }catch(SQLException e){
+                                e.printStackTrace();
+                            }
+                            System.exit(-1);
+                        }
+                    }
+
+                    reportDocBasicInfo(c, Integer.parseInt(docID));
+
+                    try{
+                        c.close();
+                    }catch(SQLException e){
+                        e.printStackTrace();
+                    }
+
+                    break;
+                case 3:
+                    String adminNum = null;
+                    tries = 5;
+
+                    while(tries > 0)
+                    {
+                        System.out.println("Enter adminNum: ");
+                        adminNum = s.nextLine();
+
+                        if(isInteger(adminNum) == -1)
+                            System.out.println("adminNum must be valid " + tries-- + " tries left");
+                        else
+                            break;
+                        if(tries == 0)
+                        {
+                            System.out.println("Too many tries");
+                            try{
+                                c.close();
+                            }catch(SQLException e){
+                                e.printStackTrace();
+                            }
+                            System.exit(-1);
+                        }
+                    }
+
+                    reportAdminInfo(c, Integer.parseInt(adminNum));
+
+                    try{
+                        c.close();
+                    }catch(SQLException e){
+                        e.printStackTrace();
+                    }
+
+                    break;
+
+                case 4:
+                    String mAdminNum = null;
+                    tries = 5;
+
+                    while(tries > 0)
+                    {
+                        System.out.println("Enter Admission Number: ");
+                        mAdminNum = s.nextLine();
+
+                        if(isInteger(mAdminNum) == -1)
+                            System.out.println("mAdminNum must be valid " + tries-- + " tries left");
+                        else
+                            break;
+                        if(tries == 0)
+                        {
+                            System.out.println("Too many tries");
+                            try{
+                                c.close();
+                            }catch(SQLException e){
+                                e.printStackTrace();
+                            }
+                            System.exit(-1);
+                        }
+                    }
+
+                    String newTotalPayment = null;
+                    tries = 5;
+
+                    while(tries > 0)
+                    {
+                        System.out.println("Enter new total payment: ");
+                        newTotalPayment = s.nextLine();
+
+                        if(isInteger(newTotalPayment) == -1)
+                            System.out.println("Total Payment must be valid " + tries-- + " tries left");
+                        else
+                            break;
+
+                        if(tries == 0)
+                        {
+                            System.out.println("Too many tries");
+                            try{
+                                c.close();
+                            }catch(SQLException e){
+                                e.printStackTrace();
+                            }
+                            System.exit(-1);
+                        }
+                    }
+
+                    updateAdminPayment(c, Integer.parseInt(mAdminNum), Integer.parseInt(newTotalPayment));
+
+                    try{
+                        c.close();
+                    }catch(SQLException e){
+                        e.printStackTrace();
+                    }
+                    break;
+                default:
+                    System.out.println("Option argument can only be from 1-4");
+                    try{
+                        c.close();
+                    }catch(SQLException e){
+                        e.printStackTrace();
+                    }
+                    System.exit(-1);
+            }
+        }
+    }
+
+    private static void reportUserOptions(){
+        System.out.println("Inside of reportUserOptions!");
+        System.out.println("Welcome to Alexander Antaya & Leo Gonsalve's Program!\n\n" +
+                "Example: >java Reporting <username> <password> [option]\n" +
+                "1 -- Report Patients Basic Information\n" +
+                "2 -- Report Doctors Basic Information\n" +
+                "3 -- Report Admissions Information\n" +
+                "4 -- Update Admissions Payment\n");
+    }
+
+    private static void reportPatientBasicInfo(Connection c, String ssn){
+        try{
+            Statement stmt = c.createStatement(); // Empty statement object
+            String result = ("SELECT * \n" +
+                    "FROM Patient \n" +
+                    "WHERE ssn = " + "'" + ssn + "'");
+            ResultSet rset = stmt.executeQuery(result);
+
+            String mSsn = "";
+            String firstName = "";
+            String lastName = "";
+            String address = "";
+
+            //The query did not find any results
+            if(!rset.isBeforeFirst()){
+                System.out.println("That query did not return any results!");
+
+                // Close Statements
+                rset.close();
+                stmt.close();
+                c.close();
+
+                System.exit(1);
+            }
+
+            // Process the results
+            while (rset.next()) { // Runs through results row by row
+                // Sets column names and assigns values to them
+                mSsn = rset.getString("ssn");
+                firstName = rset.getString("first_name");
+                lastName = rset.getString("last_name");
+                address = rset.getString("address");
+
+                if(!mSsn.equals(ssn)){
+                    System.out.println("Problem with database... Exiting.");
+                    System.exit(-1);
+                }
+
+                // Print Patient Information
+                System.out.println("\nBasic Patient Information\n" +
+                        "SSN: " + ssn + "\n" +
+                        "First Name: " + firstName + "\n" +
+                        "Last name: " + lastName + "\n" +
+                        "Address: " + address + "\n");
+            }
+
+            // Close Statements
+            rset.close();
+            stmt.close();
+            c.close();
+
+        }catch (SQLException e){
+            System.out.println("Failed to communicate with database... Exiting.");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+    private static void reportDocBasicInfo(Connection c, int doctorId){
+        try{
+            Statement stmt = c.createStatement(); // Empty statement object
+            String result = ("SELECT * \n" +
+                    "FROM Doctor \n" +
+                    "WHERE id = " + "'" + doctorId + "'");
+            ResultSet rset = stmt.executeQuery(result);
+
+            int mDoctorId = -1;
+            String firstName = "";
+            String lastName = "";
+            String gender = "";
+
+            //The query did not find any results
+            if(!rset.isBeforeFirst()){
+                System.out.println("That query did not return any results!");
+
+                // Close Statements
+                rset.close();
+                stmt.close();
+                c.close();
+
+                System.exit(1);
+            }
+
+            // Process the results
+            while (rset.next()) { // Runs through results row by row
+                mDoctorId = rset.getInt("id");
+                firstName = rset.getString("first_name");
+                lastName = rset.getString("last_name");
+                gender = rset.getString("gender");
+
+                if(mDoctorId != doctorId){
+                    System.out.println("Problem with database... Exiting.");
+                    System.exit(-1);
+                }
+
+                // Print Patient Information
+                System.out.println("\nBasic Doctor Information\n" +
+                        "Doctor ID: " + doctorId + "\n" +
+                        "First Name: " + firstName + "\n" +
+                        "Last name: " + lastName + "\n" +
+                        "Gender: " + gender + "\n");
+            }
+
+            // Close Statements
+            rset.close();
+            stmt.close();
+            c.close();
+
+        }catch (SQLException e){
+            System.out.println("Failed to communicate with database... Exiting.");
+            e.printStackTrace();
+            try {
+                c.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            System.exit(-1);
+        }
+    }
+
+    private static void reportAdminInfo(Connection c, int admissionId){
+        try{
+            //-----First Part of Query
+            Statement stmt = c.createStatement(); // Empty statement object
+            String results = ("SELECT * \n" +
+                    "FROM Admission \n" +
+                    "WHERE id = " + "'" + admissionId + "'");
+
+            ResultSet rset = stmt.executeQuery(results);
+
+            //The query did not find any results
+            if(!rset.isBeforeFirst()){
+                System.out.println("That query 1 did not return any results!");
+
+                // Close Statements
+                rset.close();
+                stmt.close();
+                c.close();
+
+                System.exit(1);
+            }
+
+            // Initialize Columns
+            String patient = "";
+            String admissionDate = "";
+            int totalPayment = -1;
+
+            // Process the results for first query
+            while (rset.next()) { // Runs through results row by row
+                patient = rset.getString("patient");
+                admissionDate = rset.getString("check_in");
+                totalPayment = rset.getInt("cost");
+            }
+
+            //-----Second Part of Query
+            stmt = c.createStatement(); // Empty statement object
+            results = ("SELECT * \n" +
+                    "FROM RoomStay \n" +
+                    "WHERE admission_id = " + "'" + admissionId + "'");
+            rset = stmt.executeQuery(results);
+
+            //The query did not find any results
+            if(!rset.isBeforeFirst()){
+                System.out.println("That query 2 did not return any results!");
+
+                // Close Statements
+                rset.close();
+                stmt.close();
+                c.close();
+
+                System.exit(1);
+            }
+
+            // Initialize Columns
+            int roomNum = -1;
+            String startTime = "";
+            String endTime = "";
+
+            // String to hold all room results in proper format
+            String rooms = "";
+
+            // Process the results for first query
+            while (rset.next()) { // Runs through results row by row
+                roomNum = rset.getInt("room_num");
+                startTime = rset.getString("start_time");
+                endTime = rset.getString("end_time");
+
+                rooms += "\tRoomNum: " + roomNum + "\tFromDate: " + startTime + "\tToDate: " + endTime + "\n";
+            }
+
+
+            //-----Third Part of Query
+            stmt = c.createStatement(); // Empty statement object
+            results = ("SELECT DISTINCT doctor_id \n" +
+                    "FROM Examination \n" +
+                    "WHERE admission_id = " + "'" + admissionId + "'");
+            rset = stmt.executeQuery(results);
+
+            //The query did not find any results
+            if(!rset.isBeforeFirst()){
+                System.out.println("That query 3 did not return any results!");
+
+                // Close Statements
+                rset.close();
+                stmt.close();
+                c.close();
+
+                System.exit(1);
+            }
+
+            // Initialize Columns
+            int doctorId = -1;
+
+            // String to hold all doctor results in proper format
+            String doctors = "";
+
+            // Process the results for first query
+            while (rset.next()) { // Runs through results row by row
+                doctorId = rset.getInt("doctor_id");
+                doctors += "\tDoctor ID: " + doctorId + "\n";
+            }
+
+            // Finished Queries, Print Admission Information
+            System.out.println("\nAdmission Information\n" +
+                    "Admission ID: " + admissionId + "\n" +
+                    "Patient SSN: " + patient + "\n" +
+                    "Admission Date (Start Date): " + admissionDate + "\n" +
+                    "Total Payment: " + totalPayment + "\n" +
+                    "Rooms: \n" +
+                    rooms +
+                    "Doctors examined the patient in this admission: \n" +
+                    doctors);
+
+            // Close Statements
+            rset.close();
+            stmt.close();
+            c.close();
+
+        }catch (SQLException e){
+            System.out.println("Failed to communicate with database... Exiting.");
+            e.printStackTrace();
+
+            try {
+                c.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            System.exit(-1);
+        }
+    }
+
+    private static void updateAdminPayment(Connection c, int admissionId, int newPaymentTotal){
+        try{
+            Statement stmt = c.createStatement(); // Empty statement object
+            String result = ("Update Admission \n" +
+                    "SET cost=" + newPaymentTotal + "\n" +
+                    "WHERE id = " + "'" + admissionId + "'");
+            int count = stmt.executeUpdate(result);
+
+            //If count is 0 then no rows were effected so we failed to update successfully
+            if(!(count > 0)){
+                System.out.println("That query did not effect any rows");
+                System.exit(1);
+            }
+
+            System.out.println("\nUpdated total cost to " + newPaymentTotal + " for Admission ID " + admissionId + "!");
+
+            // Close Statements
+            stmt.close();
+            c.close();
+
+        }catch (SQLException e){
+            System.out.println("Failed to communicate with database... Exiting.");
+            e.printStackTrace();
+
+            try {
+                c.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            System.exit(-1);
+        }
+    }
+
+    //-----Helper methods-----//
+
+    //Helper method to connect to db. Will terminate program if connection is not successful.
+    public static Connection connectToDatabase(String username, String password){
+        System.out.println("-------Oracle JDBC Connection Testing ---------");
+
+        // Register the driver
+        try { Class.forName("oracle.jdbc.driver.OracleDriver");}
+        catch (ClassNotFoundException e) {
+            System.out.println("Could not register JDBC driver");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        System.out.println("Oracle JDBC Driver Registered!");
+
+        Connection connection = null;
+
+        // Establish Connection to DB
+        try {
+            connection = DriverManager.getConnection("jdbc:oracle:thin:@oracle.wpi.edu:1521:orcl", username, password);
+        }catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        // Print Connection Success!
+        System.out.println("Oracle JDBC Driver Connected!");
+        System.out.println("Connection:" + connection);
+
+        return connection;
+    }
+
+    //Helper method to ensure that a string (usually from std_in) is a valid Integer before passing to other methods
+    public static int isInteger(String s) {
+        int retVal;
+        try {
+            retVal = Integer.parseInt(s);
+        } catch(NumberFormatException e) {
+            return -1;
+        } catch(NullPointerException e) {
+            return -1;
+        }
+        // only got here if we didn't return false
+        return retVal;
+    }
+}
+
